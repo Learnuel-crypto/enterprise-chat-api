5e1d29e52b4a0c30492231a70aee19e2963e0ef4
learnuel-crypto <linus.zoea@gmail.com>
2022-08-17 14:43:30 +0100

    This sets Up the Default account for the app
    It should be run at first before the api is fully tested
    to allow creating of other accounts

---
+++ config/config.js
@@ -0,0 +1,20 @@
+const AccountModel = require("../models/account.model");
+const UserRole = require("../models/accountrole.model");
+const { mongodbConnect } = require("./db.config");
+const { hashSync} = require("bcryptjs");
+
+ mongodbConnect();
+const pass = hashSync("nevergrowold", 12);
+ const user = new AccountModel({
+  firstname: "Learnuel",
+  lastname: "Technologies",
+  contact: "08113664697",
+  role:"Administrator",
+  username: "Learnueltech",
+  password:pass,
+ });
+user.save((err)=>{
+  if (err) return handleError(err);
+});
+ 
+  
